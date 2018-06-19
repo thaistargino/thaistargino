b702b7acd86fb3786ddb73bfa5bae00e10fdc992
thaistargino <thaistargino16@gmail.com>
2018-06-19 10:05:48 -0300

    tentativa 2

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+    $("#contact").on("click", function() {
+        alert("ops, parece que esta função ainda não está disponivel :c");
+    });
+
+debounce = function(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}; 
+
+	var $target = $('.animation'),
+			animationClass = 'animation-start',
+			offset = $(window).height() * 2/4;
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+
+		$target.each(function(){
+			var itemTop = $(this).offset().top;
+			if (documentTop > itemTop - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+
+	animeScroll();
+
+	$(document).scroll(debounce(function(){
+		animeScroll();
+	}, 200));
+})();
+
